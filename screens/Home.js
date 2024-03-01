073f19de69c4f043547dc85391f3fefed3b4c18d
Nhật Dương <duongintech@gmail.com>
2024-03-01 16:29:50 +0700

    update feature

--- screens/Home.js
+++ screens/Home.js
@@ -13,8 +13,8 @@ const LineDivider = () => {
 
 const Home = ({navigation}) => {
     const profileData = {
-        name: 'Dương Bếch',
-        point: 200,
+        name: 'Dương (bếch)',
+        point: 999,
     };
 
     const bookOtherWordsForHome = {
@@ -112,7 +112,7 @@ const Home = ({navigation}) => {
                 {/* Greetings */}
                 <View style={{flex: 1}}>
                     <View style={{marginRight: SIZES.padding}}>
-                        <Text style={{...FONTS.h3, color: COLORS.white}}>Good Morning</Text>
+                        <Text style={{...FONTS.h3, color: COLORS.white}}>Hello !</Text>
                         <Text style={{...FONTS.h2, color: COLORS.white}}>{profile.name}</Text>
                     </View>
                 </View>
@@ -320,6 +320,179 @@ const Home = ({navigation}) => {
         );
     }
 
+    function renderCategoryHeader() {
+        const renderItem = ({item}) => {
+            return (
+                <TouchableOpacity
+                    style={{flex: 1, marginRight: SIZES.padding}}
+                    onPress={() => setSelectedCategory(item.id)}>
+                    {selectedCategory == item.id && (
+                        <Text style={{...FONTS.h2, color: COLORS.white}}>{item.categoryName}</Text>
+                    )}
+                    {selectedCategory != item.id && (
+                        <Text style={{...FONTS.h2, color: COLORS.lightGray}}>{item.categoryName}</Text>
+                    )}
+                </TouchableOpacity>
+            );
+        };
+
+        return (
+            <View style={{flex: 1, paddingLeft: SIZES.padding}}>
+                <FlatList
+                    data={categories}
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={renderItem}
+                    keyExtractor={item => `${item.id}`}
+                    horizontal
+                />
+            </View>
+        );
+    }
+
+    function renderCategoryData() {
+        var books = [];
+
+        let selectedCategoryBooks = categories.filter(a => a.id == selectedCategory);
+
+        if (selectedCategoryBooks.length > 0) {
+            books = selectedCategoryBooks[0].books;
+        }
+
+        const renderItem = ({item}) => {
+            return (
+                <View style={{marginVertical: SIZES.base}}>
+                    <TouchableOpacity
+                        style={{flex: 1, flexDirection: 'row'}}
+                        onPress={() =>
+                            navigation.navigate('BookDetail', {
+                                book: item,
+                            })
+                        }>
+                        {/* Book Cover */}
+                        <Image
+                            source={item.bookCover}
+                            resizeMode="cover"
+                            style={{width: 100, height: 150, borderRadius: 10}}
+                        />
+
+                        <View style={{flex: 1, marginLeft: SIZES.radius}}>
+                            {/* Book name and author */}
+                            <View>
+                                <Text style={{paddingRight: SIZES.padding, ...FONTS.h2, color: COLORS.white}}>
+                                    {item.bookName}
+                                </Text>
+                                <Text style={{...FONTS.h3, color: COLORS.lightGray}}>{item.author}</Text>
+                            </View>
+
+                            {/* Book Info */}
+                            <View style={{flexDirection: 'row', marginTop: SIZES.radius}}>
+                                <Image
+                                    source={icons.page_filled_icon}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 20,
+                                        height: 20,
+                                        tintColor: COLORS.lightGray,
+                                    }}
+                                />
+                                <Text
+                                    style={{...FONTS.body4, color: COLORS.lightGray, paddingHorizontal: SIZES.radius}}>
+                                    {item.pageNo}
+                                </Text>
+
+                                <Image
+                                    source={icons.read_icon}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 20,
+                                        height: 20,
+                                        tintColor: COLORS.lightGray,
+                                    }}
+                                />
+                                <Text
+                                    style={{...FONTS.body4, color: COLORS.lightGray, paddingHorizontal: SIZES.radius}}>
+                                    {item.readed}
+                                </Text>
+                            </View>
+
+                            {/* Genre */}
+                            <View style={{flexDirection: 'row', marginTop: SIZES.base}}>
+                                {item.genre.includes('Adventure') && (
+                                    <View
+                                        style={{
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                            padding: SIZES.base,
+                                            marginRight: SIZES.base,
+                                            backgroundColor: COLORS.darkGreen,
+                                            height: 40,
+                                            borderRadius: SIZES.radius,
+                                        }}>
+                                        <Text style={{...FONTS.body3, color: COLORS.lightGreen}}>Adventure</Text>
+                                    </View>
+                                )}
+                                {item.genre.includes('Romance') && (
+                                    <View
+                                        style={{
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                            padding: SIZES.base,
+                                            marginRight: SIZES.base,
+                                            backgroundColor: COLORS.darkRed,
+                                            height: 40,
+                                            borderRadius: SIZES.radius,
+                                        }}>
+                                        <Text style={{...FONTS.body3, color: COLORS.lightRed}}>Romance</Text>
+                                    </View>
+                                )}
+                                {item.genre.includes('Drama') && (
+                                    <View
+                                        style={{
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                            padding: SIZES.base,
+                                            marginRight: SIZES.base,
+                                            backgroundColor: COLORS.darkBlue,
+                                            height: 40,
+                                            borderRadius: SIZES.radius,
+                                        }}>
+                                        <Text style={{...FONTS.body3, color: COLORS.lightBlue}}>Drama</Text>
+                                    </View>
+                                )}
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+
+                    {/* Bookmark Button */}
+                    <TouchableOpacity
+                        style={{position: 'absolute', top: 5, right: 15}}
+                        onPress={() => console.log('Bookmark')}>
+                        <Image
+                            source={icons.bookmark_icon}
+                            resizeMode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: COLORS.lightGray,
+                            }}
+                        />
+                    </TouchableOpacity>
+                </View>
+            );
+        };
+
+        return (
+            <View style={{flex: 1, marginTop: SIZES.radius, paddingLeft: SIZES.padding}}>
+                <FlatList
+                    data={books}
+                    renderItem={renderItem}
+                    keyExtractor={item => `${item.id}`}
+                    showsVerticalScrollIndicator={false}
+                />
+            </View>
+        );
+    }
+
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: COLORS.black}}>
             {/* Header Section */}
@@ -334,6 +507,10 @@ const Home = ({navigation}) => {
                 <View>{renderMyBookSection(myBooks)}</View>
 
                 {/* Categories Section */}
+                <View style={{marginTop: SIZES.padding}}>
+                    <View>{renderCategoryHeader()}</View>
+                    <View>{renderCategoryData()}</View>
+                </View>
             </ScrollView>
         </SafeAreaView>
     );
