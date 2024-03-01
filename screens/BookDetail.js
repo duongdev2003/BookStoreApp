6e45427ce1792c491dacb3fa2540bedfb1c32525
Dương Nhật <duongintech@gmail.com>
2024-03-01 09:41:59 +0700

    upload full

---
+++ screens/BookDetail.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+    View,
+    Text,
+    ImageBackground,
+    TouchableOpacity,
+    Image,
+    ScrollView,
+    Animated
+} from 'react-native';
+import { FONTS, COLORS, SIZES, icons } from "../constants";
+
+const BookDetail = () => {
+
+}
+
+export default BookDetail;
---
+++ screens/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+    SafeAreaView,
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+    ScrollView,
+    FlatList
+} from 'react-native';
+
+import { COLORS, FONTS, SIZES, icons, images } from '../constants';
+
+const Home = () => {
+
+}
+
+export default Home;
